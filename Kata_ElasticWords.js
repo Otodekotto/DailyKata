7aad715c503ce65ac165fbf93d18480dd983878d
Jonas D <77809544+Otodekotto@users.noreply.github.com>
2023-01-19 09:46:39 +0100

    ElasticWord
    Elastic Word Kata Logic

---
+++ Kata_ElasticWords.js
@@ -0,0 +1,43 @@
+function elasticWord(word) {
+    const arrWord = Array.from(word)
+
+    let left = ""
+    let right = ""
+    let pivot = []
+
+    console.log(arrWord)
+    if (word.length < 3) {
+        return console.log(word);
+    }
+    //odd
+    else if (word.length % 2 != 0) {
+        let firstHalfOfArray = arrWord.slice(0, arrWord.length / 2)
+        let secondHalfOfArray = arrWord.slice(arrWord.length / 2)
+        secondHalfOfArray.shift()
+        for (let i = 0; i < firstHalfOfArray.length; i++) {
+            left += firstHalfOfArray[i].repeat(i + 1)
+            right += secondHalfOfArray[i].repeat(firstHalfOfArray.length - i)
+        }
+        pivot = arrWord[Math.round(arrWord.length / 2) - 1].repeat(Math.round(arrWord.length / 2))
+
+        console.log(left + pivot + right)
+    }
+    //even
+    else {
+        let firstHalfOfArray = arrWord.slice(0, arrWord.length / 2)
+        let secondHalfOfArray = arrWord.slice(arrWord.length / 2)
+
+        for (let i = 0; i < firstHalfOfArray.length; i++) {
+            left += firstHalfOfArray[i].repeat(i + 1)
+            right += secondHalfOfArray[i].repeat(firstHalfOfArray.length - i)
+        }
+        console.log(left + right)
+
+    }
+
+}
+
+elasticWord("ANNA")
+elasticWord("KAYAK")
+elasticWord("X")
+elasticWord("DARKAKRAD")
--- Kata_MaximumProfit.js
+++ Kata_MaximumProfit.js
@@ -6,22 +6,21 @@ MaximumProfit(stockprice)
 
 //function to calculate the maximum profit
 
-function MaximumProfit(stockprice){
-    
+function MaximumProfit(stockprice) {
+
     let maximumprofit = 0;
     let newprofit = 0;
     //check the firt stockprice of the day and continue forward
-    for(let i=0; i<stockprice.length; i++){
+    for (let i = 0; i < stockprice.length; i++) {
         //check the next stockprice
-        for(let j=i+1; j<stockprice.length; j++) {
+        for (let j = i + 1; j < stockprice.length; j++) {
             //if the new stockprice is bigger then old one check out profit
-            if(stockprice[i] < stockprice[j]){
+            if (stockprice[i] < stockprice[j]) {
                 //new profit
-                newprofit = stockprice[j]-stockprice[i]
+                newprofit = stockprice[j] - stockprice[i]
             }
             //check if the new stockprice is more profittable then current maximum profit
-            if (newprofit> maximumprofit)
-            {
+            if (newprofit > maximumprofit) {
                 //new maximum profit
                 maximumprofit = newprofit;
             }
