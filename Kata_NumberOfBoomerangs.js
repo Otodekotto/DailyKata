04a8431f635e6e643b80418fe4890f7c9707f1b6
Jonas D <77809544+Otodekotto@users.noreply.github.com>
2023-01-23 08:32:14 +0100

    Number of boomerangs
    Check Array to see how many boomerangs exist in Array
    example [3, 7, 3]
    feats:  added logic to problem

---
+++ Kata_NumberOfBoomerangs.js
@@ -0,0 +1,14 @@
+function countBoomerangs(boomerangs){
+    let boomerangcount = 0;
+    for(let i = 0; i<boomerangs.length-2; i++){
+        if(boomerangs[i] === boomerangs[i+2] && boomerangs[i] !== boomerangs[i+1]){
+            boomerangcount++
+        }
+    }
+    console.log(boomerangcount)
+}
+
+countBoomerangs([9, 5, 9, 5, 1, 1, 1])
+countBoomerangs([5, 6, 6, 7, 6, 3, 9])
+countBoomerangs([4, 4, 4, 9, 9, 9, 9])
+countBoomerangs([1, 7, 1, 7, 1, 7, 1]) 
